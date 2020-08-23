af87a82d913ffbd6fa2338145eb6a05c2d9170d1
DanBi-Lee <danbi.s.rain@gmail.com>
2020-08-23 23:11:30 +0900

    날씨 추가하기

---
+++ src/js/weather.js
@@ -0,0 +1,59 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "875e80633fecba3610cdb9887449e624";
+const COORDS = 'coords';
+
+function getWeather(lat, lng){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        console.log(response);
+        return response.json()
+    }).then(function(json){
+        console.log(json);
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+
+function saveCoords(coordsObj){
+    coordsObj = JSON.stringify(coordsObj);
+    localStorage.setItem(COORDS, coordsObj);
+}
+
+function handleGeoSuccess(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError(){
+    console.log('Cant get Geo');
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCords = localStorage.getItem(COORDS);
+    console.log(loadedCords);
+    if(loadedCords === null){
+        askForCoords();
+    }else{
+        const parseCoords = JSON.parse(loadedCords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
